9b89dfa69d9661731a6e9a6fbae899297614f572
Stepasha <stepan.kot.67@mail.ru>
2025-11-18 09:42:34 +0300

    security fix

--- backend/src/main/java/ru/gigafood/backend/config/SecurityConfig.java
+++ backend/src/main/java/ru/gigafood/backend/config/SecurityConfig.java
@@ -82,6 +82,7 @@ public class SecurityConfig implements WebMvcConfigurer {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http
             .csrf(csrf -> csrf.disable())
+			.cors(Customizer.withDefaults())
 			.authorizeHttpRequests((authorize) -> authorize
 				.requestMatchers("/gigafood/api/v1/auth/**").permitAll()
 				.requestMatchers(
