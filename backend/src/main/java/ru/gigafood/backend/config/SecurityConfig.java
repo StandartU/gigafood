cc1f0d3b06e118a8955a3723f8c931751e5c6ec7
Stepasha <stepan.kot.67@mail.ru>
2025-11-24 22:10:31 +0300

    release

--- backend/src/main/java/ru/gigafood/backend/config/SecurityConfig.java
+++ backend/src/main/java/ru/gigafood/backend/config/SecurityConfig.java
@@ -106,7 +106,7 @@ public class SecurityConfig implements WebMvcConfigurer {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList("http://localhost:5500", "http://127.0.0.1:5500"));
+		config.setAllowedOrigins(Arrays.asList("*"));
 		config.setAllowedMethods(Arrays.asList("*"));
 		config.setAllowedHeaders(Arrays.asList("*"));
 		config.setAllowCredentials(true);
